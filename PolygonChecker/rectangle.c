2eb43ffbb297d61593e292f663f7bede64227917
Birendra Deuba <Bdeuba7977@conestogac.on.ca>
2024-11-17 18:28:30 -0500

    #commit4 fixed the formula of to get the perimeter of rectangle, and test cases passed.

--- PolygonChecker/rectangle.c
+++ PolygonChecker/rectangle.c
@@ -1,9 +1,9 @@
 #include "rectangle.h"
 
-int calculate_area(int length, int breadth) { //added the functionality to get the area of triangle.
+int calculate_area(int length, int breadth) { //added the functionality to get the area of rectangle.
     return length * breadth ;
 }
 
-int calculate_perimeter(int length, int breadth) { //added the functionality to get the perimeter of triangle
-    return (length + length + breadth);
+int calculate_perimeter(int length, int breadth) { //fixed the formula to get the perimeter of rectangle
+    return 2*(length + breadth);
 }
