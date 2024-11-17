46124fa6ab4b346de713b8c38afa98a6d2906509
Birendra Deuba <Bdeuba7977@conestogac.on.ca>
2024-11-17 18:20:40 -0500

    Commit#2 test case failed or aborted since it could't find the function called calculate_perimeter

--- PolygonChecker/rectangle.h
+++ PolygonChecker/rectangle.h
@@ -1,2 +1,4 @@
 #pragma
-int calculate_area(int width, int height);
+int calculate_area(int length, int breadth);
+
+int calculate_perimeter(int length, int breadth);	//added new function 
--- UnitTestForRectangle/UnitTestForRectangle.cpp
+++ UnitTestForRectangle/UnitTestForRectangle.cpp
@@ -13,17 +13,31 @@ namespace UnitTestForRectangle
 	{
 	public:
 		
+        // Test case to calulate the area of rectangle
         TEST_METHOD(TestCalculateArea)
         {
             // Arrange
-            int width = 5;
-            int height = 10;
+            int length = 5;
+            int breadth = 10;
 
             // Act
-            int area = calculate_area(width, height);
+            int area = calculate_area(length, breadth);
 
             // Assert
             Assert::AreEqual(50, area); // 5 * 10 = 50
         }
+
+        //test case to calculate the perimeter of rectangle
+
+        TEST_METHOD(TestCalculatePerimeter)
+        {
+            int length = 12;
+            int breadth = 5;
+
+            int perimeter = calculate_perimeter(length, breadth);
+
+            Assert::AreEqual(60, perimeter); // 2(l+b) = 2(12+5) = 2 * 17 = 34
+        }
+
 	};
 }
